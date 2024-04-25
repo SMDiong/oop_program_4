79fdccb75fa66a6e04b24ea5c2bbbb87a1147358
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-04-26 04:25:19 +0800

    Added pseudocodes for second exercise and added the student text file

---
+++ exercise_2/file_handling_2.py
@@ -0,0 +1,10 @@
+# Diong, Shan Marc C.
+# BSCPE 1-2
+
+# Create a text file that contains the name of 20 students with their GWA then read it
+
+# Separate the student's name and the GWA
+
+# Check the highest GWA of all the students
+
+# Output the name and GWA of the student who got the highest GWA
