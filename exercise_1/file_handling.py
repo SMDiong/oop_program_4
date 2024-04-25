d4bceae459f000aca0891f33ce13f629159a456f
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-04-26 01:22:47 +0800

    Added directories for each exercises to make it neat

--- exercise_1/file_handling.py
+++ exercise_1/file_handling.py
@@ -1,7 +1,7 @@
 # Diong, Shan Marc C.
 # BSCPE 1-2
 
-# Create a code which create atext file that contain 20 integers, name it number.txt
+# Create a code which create a text file that contain 20 integers, name it number.txt
 
 # Create a code which create a text file that contains all even numbers from extracted number.txt, name it even.txt
 
--- file_handling.py
+++
@@ -1,8 +0,0 @@
-# Diong, Shan Marc C.
-# BSCPE 1-2
-
-# Create a code which create atext file that contain 20 integers, name it number.txt
-
-# Create a code which create a text file that contains all even numbers from extracted number.txt, name it even.txt
-
-# Create a code which create a text file that contains all odd numbers extracted from number.txt, name it odd.txt
