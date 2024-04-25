e6f1782ea6ba73b4abd2ab3fb564b5a548ac8e72
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-04-26 05:57:14 +0800

    Added Text to Speech feature and colors for better output

--- exercise_2/file_handling_2.py
+++ exercise_2/file_handling_2.py
@@ -1,7 +1,14 @@
 # Diong, Shan Marc C.
 # BSCPE 1-2
 
-# Create a text file that contains the name of 20 students with their GWA then read it
+# Word Format and Text to Speech
+import pyttsx3
+text_to_speech = pyttsx3.init()
+text_to_speech.setProperty('rate', 150)
+green = "\033[0;32m"
+blue = "\033[0;34m"
+
+# Create a text file that contains the name of 20 students with their GWA then open and read it
 with open('student_name_gwa.txt', 'r') as student_file:
     lines = student_file.readlines()
 
@@ -9,7 +16,7 @@ with open('student_name_gwa.txt', 'r') as student_file:
     highest_gwa = 5.0
     highest_gwa_student = ""
 
-# Separate the student's name and the GWA
+# Separate the student's name and their GWA
 for line in lines:
     student_name, student_gwa = line.strip().split(" - GWA: ")
     student_gwa = float(student_gwa)
@@ -20,4 +27,9 @@ for line in lines:
         highest_gwa_student = student_name
 
 # Output the name and GWA of the student who got the highest GWA
-print(f"The student with the highest GWA is {highest_gwa_student} with a GWA of {highest_gwa}.")
+output_speech = f"The student with the highest GWA is {highest_gwa_student} with a GWA of {highest_gwa}."
+print(green + f"\nThe student with the highest GWA is {blue + highest_gwa_student + green} with a GWA of {blue + str(highest_gwa) + green}.")
+text_to_speech.say(output_speech)
+text_to_speech.runAndWait()
+
+# End
