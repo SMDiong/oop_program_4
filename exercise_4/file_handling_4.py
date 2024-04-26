855a1aa4742df4f5897dfd2569d8b36170533257
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-04-26 17:48:40 +0800

    Added colors and Text to Speech feature

--- exercise_4/file_handling_4.py
+++ exercise_4/file_handling_4.py
@@ -1,6 +1,13 @@
 # Diong, Shan Marc C.
 # BSCPE 1-2
 
+# Word Format and Text to Speech
+import pyttsx3
+text_to_speech = pyttsx3.init()
+text_to_speech.setProperty('rate', 150)
+green = "\033[0;32m"
+blue = "\033[0;34m"
+
 # Create the source text file named integers.txt that contains 20 integers
 
 # Create the function that will create two separate text files extracted from integers.txt
@@ -22,15 +29,20 @@ def even_odd_integers():
         for num in even_numbers:
             square = num ** 2
             double_file.write(str(square) + "\n")
-    print("Even numbers extracted and calculated")
+    print(green + "\nEven numbers extracted and calculated.")
 
 # Calculate the cubes of odd numbers then create and write it to triple.txt
     with open("triple.txt", "w") as triple_file:
         for num in odd_numbers:
             cube = num ** 3
             triple_file.write(str(cube) + "\n")
-    print("Odd numbers extracted and calculated")
+    print("\nOdd numbers extracted and calculated.")
 
 # Call the function to create the files then an output message
 even_odd_integers()
-print("Text files 'double.txt' and 'triple.txt' have been created successfully.")
+output_speech = "Text files 'double.txt' and 'triple.txt' have been created successfully."
+print(f"\nText files {blue + 'double.txt' + green} and {blue + 'triple.txt' + green} have been created successfully.")
+text_to_speech.say(output_speech)
+text_to_speech.runAndWait()
+
+# End
