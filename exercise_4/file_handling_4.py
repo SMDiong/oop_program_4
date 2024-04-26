11b1b4fc194dc46e51e442526a7dea6771291c23
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-04-26 17:26:29 +0800

    Added the pseudocodes for the fourth exercise

---
+++ exercise_4/file_handling_4.py
@@ -0,0 +1,14 @@
+# Diong, Shan Marc C.
+# BSCPE 1-2
+
+# Create the source text file named integers.txt that contains 20 integers
+
+# Create the function that will create two separate text files extracted from integers.txt
+
+# Separate the odd and even numbers
+
+# Calculate the squares of even numbers then create and write it to double.txt
+
+# Calculate the cubes of odd numbers then create and write it to triple.txt
+
+# Call the function to create the files then an output message
