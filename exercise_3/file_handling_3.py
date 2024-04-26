a666c35b6880ab381a303bf67aec0ac2ab937b8b
Shan Marc Diong <163641792+Zullivann@users.noreply.github.com>
2024-04-26 16:08:53 +0800

    Added pseudocodes for the third exercise

---
+++ exercise_3/file_handling_3.py
@@ -0,0 +1,10 @@
+# Diong, Shan Marc C.
+# BSCPE 1-2
+
+# Create a function to make a text file, name it mylife.txt
+
+# Open and write the text file
+
+# Ask the user what to write in the text file
+
+# Transfer the lines and make the text file
